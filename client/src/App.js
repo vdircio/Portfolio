302cb0aa42510394ee03ab796e25bcf327835f64
vdircio <vdirci2@illinois.edu>
2023-11-13 21:34:56 -0600

    navbar and intro

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import Navbar from './components/Navbar/navbar';
+import Intro from './components/Intro/intro';
 
 function App() {
   const [data, setData] = useState([{}])
@@ -15,10 +17,11 @@ function App() {
   }, [])
 
   return (
-    <div>
-      
+    <div className='App'>
+      <Navbar/>
+      <Intro/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ client/src/components/Intro/intro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './intro.css';
+import portrait from '../../assets/vicpic.png';
+import bc from '../../assets/briefcase.png'
+import {Link} from 'react-scroll';
+
+const Intro = () => {
+    return (
+        <section id='intro'> 
+            <div className='introContent'>
+                <span className='hello'>Hello,</span>
+                <span className='introText'>I'm <span className='introName'>Victor</span> <br/> Aspiring Developer</span>
+                <p className='introPara'>I am a student at the University of Illinois studying Mathematics and minoring in Computer Science</p>
+                <Link><button className='btn'><img src={bc} alt="hire me" className='hirebtn'/> Hire me</button></Link>
+            </div>
+            <img src={portrait} alt="Profile" className='portrait'/>
+        </section>
+    )
+}
+
+export default Intro;
