b8484743c18a5459a6688e28cd51e84109bf910f
vdircio <vdirci2@illinois.edu>
2023-11-21 13:54:04 -0600

    into

--- client/src/components/Intro/intro.js
+++ client/src/components/Intro/intro.js
@@ -10,8 +10,8 @@ const Intro = () => {
             <div className='introContent'>
                 <span className='hello'>Hello,</span>
                 <span className='introText'>I'm <span className='introName'>Victor</span> <br/> Aspiring Developer</span>
-                <p className='introPara'>I am a student at the University of Illinois studying Mathematics and minoring in Computer Science</p>
-                <Link><button className='btn'><img src={bc} alt="hire me" className='hirebtn'/> Hire me</button></Link>
+                <p className='introPara'>I am a student at the University of Illinois studying <br/>Mathematics and minoring in Computer Science</p>
+                <Link><button className='btn'><img src={bc} alt="Hire" className='hirebtn'/> Hire me</button></Link>
             </div>
             <img src={portrait} alt="Profile" className='portrait'/>
         </section>
